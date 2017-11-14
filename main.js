1cb0084c5ae0bf4b1e152931fb99b7113460a054
ttmuangkeo <tim.muangkeo@gmail.com>
2017-11-13 17:45:56 -0900

    updated js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var spanText = document.getElementsByTagName('span');
 var begin = document.getElementById('begin');
 var audio = document.getElementById('bg-music');
+var startBtn = document.getElementById('start');
+var stars = document.getElementById('stars');
+var stars2 = document.getElementById('stars2');
+var stars3 = document.getElementById('stars3');
+var title = document.getElementById('title');
+var startMenu = document.getElementById('start-menu');
 
 
 var start = function() {
@@ -8,23 +14,33 @@ var start = function() {
         if (spanText[i].innerHTML !== '') {
             spanText[i].innerHTML = '';
             begin.style.display = 'none';
-            setInterval(function() {
-                spanText[0].innerHTML = 'Hi There!'
-            }, 1500);
-            setInterval(function() {
-                spanText[1].innerHTML = 'Welcome to my Life`s Story'
-            }, 3500);
-            setInterval(function() {
-                spanText[2].innerHTML = 'So, you want to get to know me?';
-            }, 6500);
-            setInterval(function() {
-                begin.style.display = '';
-                begin.disabled = true;
-                begin.innerHTML = 'Coming soon..';
-            }, 9500);
-        }
+        };
+    };
+};
+var text1 = setInterval(function() {
+    spanText[0].innerHTML = 'Hi There!'
+}, 1500);
+var text2 = setInterval(function() {
+    spanText[1].innerHTML = 'Welcome to my Life`s Story'
+}, 3500);
+var text3 = setInterval(function() {
+    spanText[2].innerHTML = 'test text goes here';
+}, 6500);
+
+function stopInterval() {
+    clearInterval(text1);
+    clearInterval(text2);
+    clearInterval(text3);
+};
+
+
+
+var intro = function() {
+    startMenu.removeAttribute('id');
+    for (var i = 0; i <= spanText.length; i++) {
+        spanText[i].remove();
+        stopInterval()
     }
 }
 
-
-begin.addEventListener("click", start)
+begin.addEventListener("click", intro);
